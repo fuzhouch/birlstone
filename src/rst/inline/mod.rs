09d11f581a662f6e814bc6a178840a0fa799c81c
fuzhou.chen <fuzhouch@gmail.com>
2017-01-16 00:46:53 +0800

    [News] Add structure for paragraph/inline parsers.

--- src/rst/inline/mod.rs
+++ src/rst/inline/mod.rs
@@ -1,7 +1,4 @@
 // Copyright (c) 2016 Fuzhou Chen
 // Licensed under MIT 2.0 license. See LICENSE file for details.
 
-pub mod parser;
-
-mod paragraph;
-mod inline;
+//! Parser for inline markups.
--- src/rst/parser.rs
+++ src/rst/parser.rs
@@ -1,6 +1,11 @@
 // Copyright (c) 2016 Fuzhou Chen
 // Licensed under MIT 2.0 license. See LICENSE file for details.
 
+//! Define parser interface, ReStructuredText.
+
+// use super::paragraph;
+// use super::inline;
+
 const HIGHEST_ACCEPTABLE_DOCUMENT_VERSION : i32 = 1;
 
 /// Toplevel parser interface for ReStructuredText.
