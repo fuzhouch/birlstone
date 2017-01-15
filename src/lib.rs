00b34a89ebdf41cca0a0eb1efdab5a7238283802
fuzhou.chen <fuzhouch@gmail.com>
2017-01-15 23:17:21 +0800

    [News] Enable strict build checks to project.
    Don't just make it work but also make it compliant to recommended
    programming patterns.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+// Copyright (c) 2016 Fuzhou Chen
+// Licensed under MIT 2.0 license. See LICENSE file for details.
+
+#![deny(missing_docs,
+        missing_debug_implementations,
+        missing_copy_implementations,
+        trivial_casts,
+        trivial_numeric_casts,
+        unsafe_code,
+        unstable_features,
+        unused_import_braces,
+        unused_qualifications)]
+
+//! # Birlstone: RuStructuredText processing library
+//! Convert ReStructuredText documentation to HTML files.
+
 #[cfg(test)]
 mod tests {
     #[test]
